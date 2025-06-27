7887cd93e233db732767f206806ceacb8a3ab3cd
gulidagleb <160589223+gulidagleb@users.noreply.github.com>
2025-06-27 21:36:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 cached_rates = None
 rates_timestamp = None
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = "8142538757:AAFKoH3QTPZ4oydP5pPM7L9XdDdGIvkGUSc"
 
 VOLUME, WEIGHT, DELIVERY_TYPE, PACKAGING_TYPE = range(4)
 DELIVERY_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSsjTJ6lsQrm1SuD9xWWtD2PNPE3f94d9C_fQ1MO5dVt--Fl4jUsOlupp8qksdb_w/pub?gid=1485895245&single=true&output=csv"
